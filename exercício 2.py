5b9ddb4efe6f205d302bc585a83a0ed6f5c4092b
Sara <anamatos0911@gmail.com>
2026-09-03 18:49:04 -0400

    outros exercícios

---
+++ exercício 2.py
@@ -0,0 +1,10 @@
+nome_pessoa = "Ana"
+idade_da_pessoa = 19
+cidade_da_pessoa = "Manaus"
+curso_da_pessoa = "técnico em informática"
+
+print ("Olá, pessoal!")
+print ("Eu sou", nome_pessoa)
+print ("Nasci em", cidade_da_pessoa)
+print ("Tenho", idade_da_pessoa)
+print ("E estou cursando", curso_da_pessoa)
---
+++ ohayo mundo.py
@@ -0,0 +1,6 @@
+#print ("OLá, turma do")
+curso = "curso técnico em informática"
+
+print ("Olá, turma do", curso, ", estou começando a programar em python")
+
+
