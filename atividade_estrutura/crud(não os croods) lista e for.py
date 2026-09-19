08fbaa384d9cf5ebbf1421340334742ae161ed7c
Sara <anamatos0911@gmail.com>
2026-09-18 20:36:52 -0400

    main

--- atividade_estrutura/crud(não os croods) lista e for.py
+++ atividade_estrutura/crud(não os croods) lista e for.py
@@ -1,14 +1,8 @@
-biblioteca = [
-    [1, "ablubleble", "Clovis", 1960],
-    [2, "jucris", "Santoro", 2030]
-]
-
-remove_livro =int(input("Quantos livros você quer apagar?"))
-for i in range(1,remove_livro+1):
-   codigo_busca = input("Qual o código do livro") 
-   for l in remove_livro:
-     if l[0] == codigo_busca:
-        biblioteca.remove(l)
-        print("Livro excluído!")
-        break
-
+while True:
+print("1 - Cadastrar")
+print("2 - Listar")
+print("3 - Pesquisar")
+print("4 - Alterar")
+print("5 - Excluir")
+print("6 - Sair")
+opcao = input("Escolha: ")
