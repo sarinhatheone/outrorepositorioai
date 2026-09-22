30cdd91b2ff4a647ceed419b7366073c1271515e
Sara <anamatos0911@gmail.com>
2026-09-21 20:34:14 -0400

    mamain socorro

---
+++ atividade_estrutura/while.py
@@ -0,0 +1,20 @@
+'''while True:
+    resposta = input("No céu tem pães? ")
+
+    if resposta == "Nães" or "nães":
+        print("Tchães")
+    else resposta != "Nães" or "nães":
+        print (não tem")
+
+print("Ah, ok")
+'''
+while True:
+    resposta = int(input("No céu tem pães? "))
+
+    if resposta == 1 or resposta == 2:
+        print("Tchães")
+    else:
+        print ("não tem")
+        break
+
+        
