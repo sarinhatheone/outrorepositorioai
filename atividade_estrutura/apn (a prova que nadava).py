cfb2671451d72af64186d37bff08b24615cdd52c
Sara <anamatos0911@gmail.com>
2026-09-24 20:20:01 -0400

    socorro essa prova qiebrou minhas pernas

--- atividade_estrutura/apn (a prova que nadava).py
+++ atividade_estrutura/apn (a prova que nadava).py
@@ -1,4 +1,5 @@
-biblioteca=[]
+livros = []
+
 while True:
 
     print("\n===== BIBLIOTECA =====")
@@ -7,6 +8,7 @@ while True:
     print("3 - Pesquisar livro")
     print("4 - Excluir livro")
     print("5 - Sair")
+    print("6 - Mostrar quantidade de livros",)
 
     opcao = input("Digite uma opção: ")
 
@@ -14,51 +16,65 @@ while True:
 
         titulo = input("Digite o título: ")
         autor = input("Digite o autor: ")
+        
 
         livros.append([titulo, autor])
-        biblioteca.append(livros)
+
         print("Livro cadastrado!")
 
     elif opcao == "2":
 
         print("\n--- LIVROS CADASTRADOS ---")
 
-        for l in livros:
-            print("Título:", l[0])
-            print("Autor:", l[1])
+        for contador in livros:
+            print("Título:", contador[0])
+            print("Autor:", contador[1])
+           
 
     elif opcao == "3":
 
         pesquisa = input("Digite o título que deseja pesquisar: ")
-        if pesquisa == "":
-            print("Não se pode deixar vazio")
 
-        else:
-            encontrado = False
-            for livro in biblioteca:
-                if l[0] == pesquisa:
-                    print("Livro encontrado!")
-                    print("Título:", l[0])
-                    print("Autor:", l[1])
+        encontrado = False
 
+        for contador in livros:
+            if contador[0] == pesquisa:
+                print("Livro encontrado!")
+                print("Título:", contador[0])
+                print("Autor:", contador[1])
                 encontrado = True
         
-
-        
+        if  not encontrado:
+            print("Livro não encontrado")
+    #coloquei o not para se o livro não fosse encontrado desse isso dai que o senhor sabe o que é professor
+    
 
     elif opcao == "4":
 
         pesquisa = input("Digite o título que deseja excluir: ")
+        sem_retorno = False
 
         for contador in livros:
             if contador[0] == pesquisa:
                 livros.remove(contador)
                 print("Livro excluído!")
+                sem_retorno = True
+
+        if not sem_retorno:
+            print("Nenhum livro correspondente")
+    #a fórmula deu certo enyão eu vou repetir simbora
 
     elif opcao == "5":
 
         print("Programa encerrado.")
         break
+
+    elif opcao == "6":
+         print("Quantidade de livros", len(livros))
+
+        
+
+
 
     else:
 
